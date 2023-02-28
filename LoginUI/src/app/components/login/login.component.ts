70d96269e3738a542c6aaa3fa52d068d76f29c1d
Ashtorments <ayushrai420559@gmail.com>
2023-02-28 19:33:26 +0530

    validation commit

--- LoginUI/src/app/components/login/login.component.ts
+++ LoginUI/src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import validateforms from 'src/app/helpers/validateforms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+loginForm!: FormGroup;
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.loginForm= this.fb.group({
+      email: ['',[Validators.required, Validators.email]],
+      password: ['',Validators.required]
+    })
   }
 
+  onSubmit(){
+    if(this.loginForm.valid){
+
+      console.log(this.loginForm.value)
+      //send the obj to DB
+    }
+    else{
+      console.log("form is not valid")
+      validateforms.validateAllFormFields(this.loginForm);
+      alert("Invalid")
+      //throw error with required fields
+    }
+  }
+  
 }
--- LoginUI/src/app/components/signup/signup.component.ts
+++ LoginUI/src/app/components/signup/signup.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import validateforms from 'src/app/helpers/validateforms';
 
 @Component({
   selector: 'app-signup',
@@ -6,10 +8,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent implements OnInit {
-
-  constructor() { }
+  signupForm!: FormGroup;
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.signupForm= this.fb.group({
+      email: ['',[Validators.required, Validators.email]],
+      password: ['',Validators.required],
+      Fname: ['',Validators.required],
+      Lname: ['',Validators.required],
+
+    })
   }
 
+  onSubmit(){
+    if(this.signupForm.valid){
+
+      console.log(this.signupForm.value)
+      //send the obj to DB
+    }
+    else{
+      console.log("form is not valid")
+      this.validateAllFormFields(this.signupForm);
+      alert("Invalid")
+      //throw error with required fields
+    }
+  }
+  private validateAllFormFields(formGroup:FormGroup){
+    Object.keys(formGroup.controls).forEach(field=>{
+      const control = formGroup.get(field);
+      if(control instanceof FormControl){
+        control.markAsDirty({onlySelf:true});
+      } else if (control instanceof FormGroup){
+        validateforms.validateAllFormFields(control)
+      }
+    })
+  }
 }
